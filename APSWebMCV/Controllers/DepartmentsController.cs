7513677fb82261a9ef74a9008731a18601586373
Gabriel Okumura <Gabrielokumura2017.gmail.com>
2021-11-16 09:36:10 -0300

    First Model-Controller-View - Department

---
+++ APSWebMCV/Controllers/DepartmentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APSWebMCV.Models;
+
+namespace APSWebMCV.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Ciencia da Computação" });
+            list.Add(new Department { Id = 1, Name = "Administração" });
+
+
+            return View(list);
+        }
+    }
+}
