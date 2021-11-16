01b5838aba6748fd71afd467ced8e195740d9ec3
Gabriel Okumura <Gabrielokumura2017.gmail.com>
2021-11-16 11:02:56 -0300

    Adaptação para MySQL e primeira migration

--- APSWebMCV/Startup.cs
+++ APSWebMCV/Startup.cs
@@ -37,7 +37,8 @@ namespace APSWebMCV
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<APSWebMCVContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("APSWebMCVContext")));
+                    options.UseMySql(Configuration.GetConnectionString("APSWebMCVContext"), builder =>
+                        builder.MigrationsAssembly("APSWebMCV")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
