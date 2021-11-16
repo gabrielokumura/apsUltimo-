a4c82793aaba8cf1adeca22d08571a235dd93ee3
Gabriel Okumura <Gabrielokumura2017.gmail.com>
2021-11-16 10:04:01 -0300

    CRUD Scaffolding

---
+++ APSWebMCV/Data/APSWebMCVContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APSWebMCV.Models;
+
+namespace APSWebMCV.Data
+{
+    public class APSWebMCVContext : DbContext
+    {
+        public APSWebMCVContext (DbContextOptions<APSWebMCVContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APSWebMCV.Models.Department> Department { get; set; }
+    }
+}
